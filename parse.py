407b1d5bbdb473c485da032b872311e694a1c9b3
Ryan Allen <rmallensb@gmail.com>
2018-03-09 18:33:03 -0800

    Threading was a bust, parse.py is what we will use

--- parse.py
+++ parse.py
@@ -1,4 +1,3 @@
-from threading import Thread, Lock
 import sys, os
 import pyshark
 import json
@@ -15,9 +14,6 @@ output_a = 'gen_a.txt'
 output_g = 'gen_g.txt'
 output_n = 'gen_n.txt'
 
-a_lock = Lock()
-g_lock = Lock()
-n_lock = Lock()
 
 # General parser template
 def gen_template():
@@ -46,63 +42,50 @@ def merger(d1, d2):
 
 def write(d, type):
     if type == 'a':
-        with a_lock:
-            path = Path(output_a)
-            if path.is_file():
-                with open(output_a, 'r') as fa:
-                    data = json.load(fa)
-                    new_data = merger(d, data)
-            else:
-                os.system('touch {}'.format(output_a))
-                new_data = d
+        path = Path(output_a)
+        if path.is_file():
+            with open(output_a, 'r') as fa:
+                data = json.load(fa)
+                new_data = merger(d, data)
+        else:
+            os.system('touch {}'.format(output_a))
+            new_data = d
 
-            with open(output_a, 'w') as fa:
-                fa.write(json.dumps(new_data, indent=2))
+        with open(output_a, 'w') as fa:
+            fa.write(json.dumps(new_data, indent=2))
 
     if type == 'g':
-        with g_lock:
-            path = Path(output_g)
-            if path.is_file():
-                with open(output_g, 'r') as fg:
-                    data = json.load(fg)
-                    new_data = merger(d, data)
-            else:
-                os.system('touch {}'.format(output_g))
-                new_data = d
+        path = Path(output_g)
+        if path.is_file():
+            with open(output_g, 'r') as fg:
+                data = json.load(fg)
+                new_data = merger(d, data)
+        else:
+            os.system('touch {}'.format(output_g))
+            new_data = d
 
-            with open(output_g, 'w') as fg:
-                fg.write(json.dumps(new_data, indent=2))
+        with open(output_g, 'w') as fg:
+            fg.write(json.dumps(new_data, indent=2))
     
     if type == 'n':
-        with n_lock:
-            path = Path(output_n)
-            if path.is_file():
-                with open(output_n, 'r') as fn:
-                    data = json.load(fn)
-                    new_data = merger(d, data)
-            else:
-                os.system('touch {}'.format(output_n))
-                new_data = d
+        path = Path(output_n)
+        if path.is_file():
+            with open(output_n, 'r') as fn:
+                data = json.load(fn)
+                new_data = merger(d, data)
+        else:
+            os.system('touch {}'.format(output_n))
+            new_data = d
 
-            with open(output_n, 'w') as fn:
-                fn.write(json.dumps(new_data, indent=2))
+        with open(output_n, 'w') as fn:
+            fn.write(json.dumps(new_data, indent=2))
 
 def threader(pcap):
     (a, g, n) = splitter(pcap) 
 
-    ta = Thread(target=write, args=(a, 'a'))
-    ta.run()
-    tg = Thread(target=write, args=(g, 'g'))
-    tg.run()
-    tn = Thread(target=write, args=(n, 'n'))
-    tn.run()
-
-    if ta.is_alive():
-        ta.join()
-    if tg.is_alive():
-        tg.join()
-    if tn.is_alive():
-        tn.join()
+    write(a, 'a')
+    write(g, 'g')
+    write(n, 'n')
 
 
 def splitter(path):
@@ -148,19 +131,10 @@ def main():
         for root, dirs, files in os.walk(directory):
             for file in files:
                 path = '{0}{1}'.format(directory, file)
-                t = Thread(target=threader, args=(path,))
-                #t.dameon = True
-                t_list.append(t)
-                t.run()
-                print 'thread kicked off'
-
-    for thread in t_list:
-        if thread.is_alive():
-            thread.join()
-                
+                threader(path)
 
     
-    print 'All threads finished.'
+    print 'Done.'
     exit(0)
 
 if __name__ == "__main__":
